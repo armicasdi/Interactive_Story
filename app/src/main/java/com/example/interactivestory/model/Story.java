c547db65a4757dbf63ab36379a00c051848d9398
Arturo Castro <armicasdi@gmail.com>
2019-10-02 16:20:48 -0600

    Story Issue

--- app/src/main/java/com/example/interactivestory/model/Story.java
+++ app/src/main/java/com/example/interactivestory/model/Story.java
@@ -7,6 +7,8 @@ public class Story {
 
     public Story(){
 
+        pages = new Page[7];
+
 
         pages[0] = new Page(R.drawable.page0, R.string.page0, new Choice(R.string.page0_choice1, 1), new Choice(R.string.page0_choice2, 2) );
 
